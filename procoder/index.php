8c671062f204874aa3611f6ee4420e3850657903
Gazi Ehsanul Haque <antik.edu@gmail.com>
2023-08-21 08:20:48 +0600

    main

---
+++ procoder/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP document</title>
+</head>
+<body>
+    <?php
+    $food = "PIZZA";
+    echo 'I love to eat '. $food;  ?>
+</body>
+</html>
